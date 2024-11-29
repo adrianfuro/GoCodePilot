776ef1c96e301087a21a726239ba8c56405ad4aa
adrianfuro <aady81@outlook.com>
2024-11-29 15:39:54 +0200

    added cobra to handle cli commands parsing

--- main.go
+++ main.go
@@ -1,40 +1,9 @@
 package main
 
 import (
-	//"fmt"
-	"log"
-	"os"
-	//"strings"
-
 	"github.com/adrianfuro/GoCodePilot/cmd"
-	//	"github.com/adrianfuro/GoCodePilot/internal/ollama"
-	//	"github.com/adrianfuro/GoCodePilot/internal/openai"
-	"github.com/joho/godotenv"
+	"os"
 )
-
-type EnvVars struct {
-	LLMProvider  string
-	SystemPrompt string
-	OpenAIKey    string
-	Model        string
-	Temperature  string
-	Tokens       string
-}
-
-func LoadEnvVars() *EnvVars {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	return &EnvVars{
-		LLMProvider:  os.Getenv("LLM_PROVIDER"),
-		SystemPrompt: os.Getenv("SYSTEM_PROMPT"),
-		OpenAIKey:    os.Getenv("OPENAI_KEY"),
-		Model:        os.Getenv("MODEL"),
-		Temperature:  os.Getenv("TEMPERATURE"),
-		Tokens:       os.Getenv("TOKENS"),
-	}
-}
 
 func main() {
 	if err := cmd.RootCommand.Execute(); err != nil {
